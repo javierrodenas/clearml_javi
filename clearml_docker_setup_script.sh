931aa50cf5ab1cd3b353b87b769f88182fc52c88
javi <javierrodenascumplido@gmail.com>
2021-07-28 15:56:39 +0200

    requirements

--- clearml_docker_setup_script.sh
+++ clearml_docker_setup_script.sh
@@ -1,4 +1,3 @@
-wandb login 126df13111573362ac4698f1eacd2e1b31e50a45
 mkdir -p /datasets/iccv
 clearml-data get --id 04fe1e704c1a424bbe941d8f95f23fd8
 ln -s /clearml_agent_cache/storage_manager/datasets/ds_04fe1e704c1a424bbe941d8f95f23fd8 /datasets/iccv/val
@@ -6,3 +5,4 @@ clearml-data get --id ac705e35976947d3ace2283fbb11a87b
 ln -s  /clearml_agent_cache/storage_manager/datasets/ds_ac705e35976947d3ace2283fbb11a87b /datasets/iccv/val
 clearml-data get --id 2f8e31b466494869bddc88d100eded5c
 ln -s /clearml_agent_cache/storage_manager/datasets/ds_2f8e31b466494869bddc88d100eded5c /datasets/iccv/train
+pip install tlt
--- clearml_launch_experiment.sh
+++ clearml_launch_experiment.sh
@@ -3,7 +3,6 @@ clearml-task \
 --name "Test" \
 --docker erenbalatkan/pytorch:1.9 \
 --docker_bash_setup_script clearml_docker_setup_script.sh \
---docker_args="--shm-size=8g" \
---requirements requirements.txt \
+--docker_args="--shm-size=16g --ipc=host -v /media:/media" \
 --script clearml_experiment_runner.py \
 --queue ub_196
